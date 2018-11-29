4cabda946200268bd2e4ee38f850dbaed020f08e
Josega149 <jg.tamura10@uniandes.edu.co>
2018-11-29 12:00:40 -0500

    logic done

--- unacloud_fuzzyLogic.js
+++ unacloud_fuzzyLogic.js
@@ -1,32 +1,18 @@
 let fuzzylogic = require('fuzzylogic');
 
-function threatCalc (threat) {
-    var cpu_low = fuzzylogic.triangle(threat, 0, 20, 40);
-    var cpu_N  = fuzzylogic.trapezoid(threat, 20, 30, 90, 100);
-    var cpu_high = fuzzylogic.grade(threat, 90, 100);
-    
-    console.log('Threat: ' + threat);
-    console.log('no attack: '       + probabNoAttack);
-    console.log('normal attack: '   + probabNormalAttack);
-    console.log('enraged attack: '  + probabEnragedAttack);
-};
+function threatCalc (machine) {
+    const diskRisk = fuzzylogic.trapezoid(machine.unacloudDisk.percent, 20, 90, 100, 101);
+    const cpuRisk = fuzzylogic.trapezoid(machine.cpu, 0, 90, 100, 101);
+    const ramRisk = fuzzylogic.grade(machine.ram.percent, 0, 100) - 0.3;
 
-threatCalc(10);
-console.log("\n");
-threatCalc(20);
-console.log("\n");
-threatCalc(30);
-console.log("\n");
-threatCalc(40);
-console.log("\n");
-threatCalc(50);
-console.log("\n");
-threatCalc(60);
-console.log("\n");
-threatCalc(70);
-console.log("\n");
-threatCalc(80);
-console.log("\n");
-threatCalc(91);
-console.log("\n");
-threatCalc(100);
+    console.log('disk: ' + diskRisk);
+    console.log('cpu: '  + cpuRisk);
+    console.log('ram: '  + ramRisk);
+    return Math.max(cpuRisk, Math.max(diskRisk, ramRisk));
+};
+let machine = {
+    unacloudDisk : { percent: 70},
+    cpu: 50,
+    ram : { percent: 70 }
+}
+console.log(threatCalc(machine));
